106f866962ed5bd8b7f56128421745b1792e0f22
Harpreet Singh <singhsharpreet3@gmail.com>
2026-01-13 22:08:04 +0530

    Initial Commit

---
+++ Pinterest/PinterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestApp.swift
+//  Pinterest
+//
+//  Created by Harpreet Singh on 13/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PinterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
